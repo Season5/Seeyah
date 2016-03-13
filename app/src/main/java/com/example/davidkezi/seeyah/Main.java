9b0a74888b076341d1fd632023be6375157270b8
mrkezii <david.kezi@yahoo.com>
2016-03-13 20:31:43 +0300

    Splash, app intro and main added

---
+++ app/src/main/java/com/example/davidkezi/seeyah/Main.java
@@ -0,0 +1,8 @@
+package com.example.davidkezi.seeyah;
+/**
+ * Created by davidkezi on 12/03/2016.
+ */
+import android.support.v7.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+}
