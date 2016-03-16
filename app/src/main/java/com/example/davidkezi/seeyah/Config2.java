462246843c0715b3c75004c140ef9a9e65e45718
mrkezii <david.kezi@yahoo.com>
2016-03-16 12:51:15 +0300

    merging

---
+++ app/src/main/java/com/example/davidkezi/seeyah/Config2.java
@@ -0,0 +1,10 @@
+package com.example.davidkezi.seeyah;
+
+/**
+ * Created by echessa on 6/17/15.
+ */
+public class Config2 {
+
+    public static final String CLIENT_ID = "a0b34eaecb2b7d1955e848f71536343a";
+    public static final String API_URL = "https://api.soundcloud.com";
+}
--- app/src/main/java/com/example/davidkezi/seeyah/MainActivity.java
+++ app/src/main/java/com/example/davidkezi/seeyah/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.davidkezi.seeyah;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -19,6 +21,17 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
     private YouTubeThumbnailView youTubeThumbnailView;
     private MyPlaybackEventListener myPlaybackEventListener;
     private MyPlayerStateChangeListener myPlayerStateChangeListener;
+
+    public void clickHere(View v){
+//        Button button=  (Button)findViewById(R.id.sound);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getBaseContext(), MainActivity2.class));
+//            }
+//        });
+        startActivity(new Intent(getBaseContext(), MainActivity2.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -140,5 +153,6 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
             // Called when errors occur
         }
     }
+
 
 }
